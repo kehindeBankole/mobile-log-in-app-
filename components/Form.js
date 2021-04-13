e4d61de1ddd4bf9a2a8b975f8268a0b93c2d5d96
kehinde <bankolek1@gmail.com>
2021-04-13 14:51:03 +0100

    mobile login implementation done

--- components/Form.js
+++ components/Form.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function Form() {
     return (
         <View>
             <Text style={styles.textConatiner}>Learn Graphic and UI/UX designing in Hindi
 for free with live projects.</Text>
+            <View style={{ marginTop: 20 }}>
+                <TextInput
+                    style={{ paddingHorizontal: 20, height: 60, backgroundColor: '#FFFFFF', fontSize: 16, borderRadius: 40 }}
+                    placeholder="Email Address"
+
+                />
+                <TextInput
+                    style={{ marginTop: 20, paddingHorizontal: 20, height: 60, backgroundColor: '#FFFFFF', fontSize: 16, borderRadius: 40 }}
+                    placeholder="Password"
+
+                />
+                <Pressable
+                    onPress={() => alert('12')}
+                    style={{
+                        backgroundColor: '#FD8C00',
+                        height: 60,
+                        width: '100%',
+                        borderRadius: 40,
+                        justifyContent: 'center',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginTop: 20,
+                    }}>
+                    <Text style={{ textTransform: 'uppercase', color: '#FFFFFF', fontSize: 16 }}>login</Text>
+                </Pressable>
+                <Text style={{ textAlign: 'right', fontSize: 14, marginTop: 20, color: '#FD8C00' }}>Forgot Password?</Text>
+                <View style={{ }}>
+                    <Text style={{ textAlign: 'center', fontSize: 14, marginTop: 20, color: '#FFFFFF' }}>or login with</Text>
+                    <View style={{ justifyContent: 'center' , flexDirection:'row' , marginTop:20 }}>
+                        <Image source={require('../assets/gmail.png')} style={{marginRight:15}}/>
+                        <Image source={require('../assets/facebook.png')} style={{marginRight:15}}/>
+                        <Image source={require('../assets/twitter.png')} />
+                    </View>
+                   
+                    
+                    <Text style={{textAlign:'center' , marginTop:20 , fontSize:12 , color:'white'}}>Don’t have an account? <Text style={{color:"#FD8C00"}}>Register now</Text></Text>
+               <Text style={{textAlign:'center' , marginTop:20 , fontSize:13 , color:'white' , lineHeight:30}}>By signing up, you are agree with our <Text style={{color:"#FD8C00"}}>Terms & Conditions</Text></Text>
+               
+                </View>
+            </View>
         </View>
     )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
+import { StyleSheet, Text, View, Image, Dimensions, ScrollView } from 'react-native'
 import Form from '../components/Form'
 
 export default function Login() {
     return (
-        <View style={styles.loginContainer}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.loginContainer}>
             <View style={styles.imageContainer}>
                 <Image source={require('../assets/Group.png')} resizeMode='contain' />
             </View>
-            <View style={{ flex: 3 }}>
+            <View style={{ flex: 4 }}>
                 <Form />
             </View>
         </View>
+        </ScrollView>
     )
 }
 
@@ -19,12 +21,14 @@ const styles = StyleSheet.create({
     loginContainer: {
         paddingTop: 40,
         paddingHorizontal: 40,
-        height: Dimensions.get('window').height,
-        backgroundColor: "#81248A"
+        // height: Dimensions.get('window').height,
+        backgroundColor: "#81248A",
+        height:'100%'
     },
     imageContainer: {
         flex: 1,
         justifyContent: 'center',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginBottom:20
     }
 })
